7896094d939a113a838ab4c18a2ffebde660b20b
Alvaro Giner <aginer079j@gmail.com>
2025-03-18 19:45:02 +0100

    button component finsihed

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,13 @@ function App() {
   return (
     <>
       <Button className="btn" text="text" />
-      <Button className="btn btn--outlined" text="text" />
+      <Button className="btn" text="text" />
+      <Button className="btn btn--outlined" text="Create Event" />
+      <Button className="btn btn--outlined" text="text" disabled />
       <Button className="btn btn--secondary" text="text" />
+      <Button className="btn btn--secondary" text="text" disabled />
       <Button className="btn btn--secondary__outlined" text="text" />
+      <Button className="btn btn--secondary__outlined" text="text" disabled />
     </>
   );
 }
--- src/button.jsx
+++ src/button.jsx
@@ -1,3 +1,4 @@
+import "./index.css";
 import "./buttons.css";
 
 export const Button = (props) => {
