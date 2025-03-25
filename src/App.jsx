db45dddc76780469125bb3589ba677266bc25023
alvaroginer <aginer079j@gmail.com>
2025-03-25 20:02:37 +0100

    excersise

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,11 @@
 import "./App.css";
 import "./index.css";
-import { Button } from "./button";
-import { Card } from "./Card";
+import { Tasks } from "./tasks/Tasks";
 
 function App() {
   return (
     <>
-      <Button className="btn" text="text" />
-      <Button className="btn" text="text" />
-      <Button className="btn btn--outlined" text="Create Event" />
-      <Button className="btn btn--outlined" text="text" disabled />
-      <Button className="btn btn--secondary" text="text" />
-      <Button className="btn btn--secondary" text="text" disabled />
-      <Button className="btn btn--secondary__outlined" text="text" />
-      <Button className="btn btn--secondary__outlined" text="text" disabled />
-      <Card />
+      <Tasks />
     </>
   );
 }
